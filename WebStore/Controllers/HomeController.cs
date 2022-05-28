b1991721d4cb3897b373c7941f8711d7f813dcb7
Maksim Bogatov <maxbog@inbox.ru>
2022-05-28 18:57:11 +0300

    Добавьте файлы проекта.

---
+++ WebStore/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebStore.Models;
+
+namespace WebStore.Controllers;
+
+public class HomeController : Controller
+{
+    private static readonly List<Employee> __Employees = new()
+    {
+        new Employee { Id = 1, LastName = "Иванов", FirstName = "Иван", Patronymic = "Иванович", Age = 23 },
+        new Employee { Id = 2, LastName = "Петров", FirstName = "Пётр", Patronymic = "Петрович", Age = 27 },
+        new Employee { Id = 3, LastName = "Сидоров", FirstName = "Сидор", Patronymic = "Сидорович", Age = 18 },
+    };
+
+    public IActionResult Index()
+    {
+        return View();
+        //return Content("Index action");
+        //return new ViewResult();
+    }
+
+    public IActionResult Greetings(string? id)
+    {
+        return Content($"Hello from first controller - {id}");
+    }
+
+    public IActionResult Employees()
+    {
+        return View(__Employees);
+    }
+
+    public IActionResult EmployeDetails(int Id)
+    {
+        var employee = __Employees.FirstOrDefault(x => x.Id == Id);
+        if (employee is null)
+            return NotFound();
+
+        return View(employee);
+    }
+}
